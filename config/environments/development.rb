744d911ea4f2bf8e762d8f501c0416c10d8f8ee8
Yogen Aggarwal <yogenaggarwal@Yogens-MacBook-Pro.local>
2021-02-25 18:38:18 +0530

    Add email config for prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
-      :user_name            => 'tmstudent4@gmail.com',
-      :password             => 'Pass123$',
+      :user_name            => ENV["GMAIL_USERNAME"],
+      :password             => ENV["GMAIL_PASSWORD"],
       :authentication       => "plain",
       :enable_starttls_auto => true,
       openssl_verify_mode: "none"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,13 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => ENV["GMAIL_USERNAME"],
+      :password             => ENV["GMAIL_PASSWORD"],
+      :authentication       => "plain",
+      :enable_starttls_auto => true,
+      openssl_verify_mode: "none"
+  }
 end

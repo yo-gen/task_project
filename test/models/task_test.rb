1d3668b4af50792f01dd52f0041d066567d830cd
Yogen Aggarwal <yogenaggarwal@Yogens-MacBook-Pro.local>
2021-02-25 10:12:50 +0530

    Add task scaffold and basic validations and model test

---
+++ test/models/task_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class TaskTest < ActiveSupport::TestCase
+  test "should not save task without name" do
+    task = Task.new(description: "This is first task", deadline: Time.now, user: users(:user1))
+    assert_not task.save
+  end
+
+  test "should not save task without deadline" do
+    task = Task.new(name: "New task", description: "This is first task", user: users(:user1))
+    assert_not task.save
+  end
+
+  test "should save task with name and deadline" do
+    task = Task.new(name: "New Task", deadline: Time.now, user: users(:user1))
+    assert task.save
+  end
+end
